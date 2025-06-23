24ac8b42346e61a2fd2daea3938572fa44e8211f
Vaishnavi Patil <vaishnavipatil3443@gmail.com>
2025-06-23 23:08:10 +0530

    Update PringArrayUsingForLoop.java

--- PringArrayUsingForLoop.java
+++ PringArrayUsingForLoop.java
@@ -14,7 +14,6 @@ public class PringArrayUsingForLoop {
         //backward tracing
            for(int i = arr.length-1; i >= 0; i--){
             System.out.print(arr[i] + " ");
-        }
-    
-    }
- }
+           }
+     }
+}
